d3e21c7e1d3253ad8e311e1c993596a30aed3364
quentinrouger <quentin_rouger@hotmail.fr>
2024-01-12 11:15:10 +0100

    Fix null check in insert_node function

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -12,7 +12,7 @@ listint_t *insert_node(listint_t **head, int number)
 {
     listint_t *new_node, *current, *prev;
 
-    if (!head)
+    if (head == NULL)
         return (NULL);
 
     new_node = malloc(sizeof(listint_t));
