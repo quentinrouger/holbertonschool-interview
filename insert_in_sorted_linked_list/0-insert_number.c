42eb8cc482218096ab301ce52df05c6cd5c9d11f
quentinrouger <quentin_rouger@hotmail.fr>
2024-01-12 11:07:04 +0100

    Add null check for head pointer

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -12,6 +12,9 @@ listint_t *insert_node(listint_t **head, int number)
 {
     listint_t *new_node, *current, *prev;
 
+    if (!head)
+        return (NULL);
+
     new_node = malloc(sizeof(listint_t));
     if (new_node == NULL)
         return (NULL);
