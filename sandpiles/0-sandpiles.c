78fd1179f71d2f81c85fe48222473080e0a644ab
quentinrouger <quentin_rouger@hotmail.fr>
2024-02-02 10:57:34 +0100

    Fix: spacing between numbers

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -11,7 +11,10 @@ void print_grid(int grid[3][3]) {
     int i, j;
     for (i = 0; i < 3; i++) {
         for (j = 0; j < 3; j++) {
-            printf("%d ", grid[i][j]);
+            printf("%d", grid[i][j]);
+            if (j < 2) {
+                printf(" ");
+            }
         }
         printf("\n");
     }
