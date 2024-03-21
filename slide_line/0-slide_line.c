47e04d4c81444a3ceea3441f0f910be9d3620d76
quentinrouger <quentin_rouger@hotmail.fr>
2024-03-21 12:33:13 +0100

    Add: Task 0

--- slide_line/0-slide_line.c
+++ slide_line/0-slide_line.c
@@ -1,77 +1,90 @@
 #include "slide_line.h"
+#include <stddef.h>
 
 /**
- * slide_line - slides and merges an array of integers
- * @line: array of integers
- * @size: size of the array
- * @direction: direction to slide and merge (SLIDE_LEFT or SLIDE_RIGHT)
- * Return: 1 if successful, 0 otherwise
+ * slide_line - Slides and merges an array of integers
+ *              towards a given direction
+ * @line: Array of integers containing size elements
+ * @size: Number of elements in @line
+ * @direction: Direction to slide the elements
+ *
+ * Return: 1 upon success, 0 upon failure
  */
 int slide_line(int *line, size_t size, int direction)
 {
-    size_t i, j, k;
+	size_t i, j;
+	int merged = 0;
 
-    if (direction != SLIDE_LEFT && direction != SLIDE_RIGHT)
-        return (0);
+	if (line == NULL || (direction != SLIDE_LEFT && direction != SLIDE_RIGHT))
+		return (0);
 
-    if (direction == SLIDE_LEFT)
-    {
-        for (i = 0; i < size; ++i)
-        {
-            if (line[i] == 0)
-            {
-                for (j = i + 1; j < size && line[j] == 0; ++j)
-                    ;
-                if (j == size)
-                    break;
-                line[i] = line[j];
-                line[j] = 0;
-            }
-            for (k = i + 1; k < size; ++k)
-            {
-                if (line[k] != 0)
-                {
-                    if (line[i] == line[k])
-                    {
-                        line[i] += line[k];
-                        line[k] = 0;
-                        break;
-                    }
-                    else
-                        break;
-                }
-            }
-        }
-    }
-    else if (direction == SLIDE_RIGHT)
-    {
-        for (i = size - 1; i < size; --i)
-        {
-            if (line[i] == 0)
-            {
-                for (j = i - 1; j >= 0 && line[j] == 0; --j)
-                    ;
-                if (j == (size_t)-1)
-                    break;
-                line[i] = line[j];
-                line[j] = 0;
-            }
-            for (k = i - 1; k >= 0; --k)
-            {
-                if (line[k] != 0)
-                {
-                    if (line[i] == line[k])
-                    {
-                        line[i] += line[k];
-                        line[k] = 0;
-                        break;
-                    }
-                    else
-                        break;
-                }
-            }
-        }
-    }
-
-    return (1);
+	if (direction == SLIDE_LEFT)
+	{
+		for (i = 0; i < size; ++i)
+		{
+			if (line[i] == 0)
+				continue;
+			for (j = i + 1; j < size; ++j)
+			{
+				if (line[j] != 0)
+				{
+					if (line[i] == line[j] && !merged)
+					{
+						line[i] *= 2;
+						line[j] = 0;
+						merged = 1;
+					}
+					break;
+				}
+			}
+			merged = 0;
+		}
+		for (i = 0, j = 0; i < size; ++i)
+		{
+			if (line[i] != 0)
+			{
+				if (i != j)
+				{
+					line[j] = line[i];
+					line[i] = 0;
+				}
+				++j;
+			}
+		}
+	}
+	else if (direction == SLIDE_RIGHT)
+	{
+		for (i = size; i > 0; --i)
+		{
+			if (line[i - 1] == 0)
+				continue;
+			for (j = i - 1; j > 0; --j)
+			{
+				if (line[j - 1] != 0)
+				{
+					if (line[i - 1] == line[j - 1] && !merged)
+					{
+						line[i - 1] *= 2;
+						line[j - 1] = 0;
+						merged = 1;
+					}
+					break;
+				}
+			}
+			merged = 0;
+		}
+		for (i = size, j = size; i > 0; --i)
+		{
+			if (line[i - 1] != 0)
+			{
+				if (i != j)
+				{
+					line[j - 1] = line[i - 1];
+					line[i - 1] = 0;
+				}
+				--j;
+			}
+		}
+	}
+	return (1);
 }
