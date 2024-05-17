431ed4a7f5cf2664688ad822115cfba004baaa8b
quentinrouger <quentin_rouger@hotmail.fr>
2024-05-17 10:22:58 +0200

    Implement advanced binary search algorithm with recursive helper function

---
+++ advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+/**
+ * print_array - Prints the array being searched
+ * @array: The array to be printed
+ * @left: The starting index
+ * @right: The ending index
+ */
+void print_array(int *array, size_t left, size_t right)
+{
+    size_t i;
+
+    printf("Searching in array: ");
+    for (i = left; i <= right; i++)
+    {
+        printf("%d", array[i]);
+        if (i < right)
+            printf(", ");
+    }
+    printf("\n");
+}
+
+/**
+ * advanced_binary_recursive - Recursive function to perform advanced binary search
+ * @array: The array to search in
+ * @left: The starting index of the subarray to search
+ * @right: The ending index of the subarray to search
+ * @value: The value to search for
+ *
+ * Return: The index where the value is located, or -1 if not found
+ */
+int advanced_binary_recursive(int *array, size_t left, size_t right, int value)
+{
+    size_t mid;
+
+    if (left > right)
+        return (-1);
+
+    print_array(array, left, right);
+
+    mid = left + (right - left) / 2;
+
+    if (array[mid] == value && (mid == left || array[mid - 1] != value))
+        return (mid);
+
+    if (array[mid] >= value)
+        return (advanced_binary_recursive(array, left, mid, value));
+    return (advanced_binary_recursive(array, mid + 1, right, value));
+}
+
+/**
+ * advanced_binary - Searches for a value in a sorted array of integers
+ * @array: The array to search in
+ * @size: The number of elements in the array
+ * @value: The value to search for
+ *
+ * Return: The index where the value is located, or -1 if not found
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+    if (array == NULL || size == 0)
+        return (-1);
+    return (advanced_binary_recursive(array, 0, size - 1, value));
+}
