2146bdbc001356302cea29a9a6784aac5e45f569
quentinrouger <quentin_rouger@hotmail.fr>
2024-06-28 17:39:54 +0200

    Fix betty coding style

--- find_the_loop/0-find_loop.c
+++ find_the_loop/0-find_loop.c
@@ -11,21 +11,21 @@ listint_t *find_listint_loop(listint_t *head)
 		listint_t *slow = head;
 		listint_t *fast = head;
 
-		while (slow && fast && fast->next)
-		{
-				slow = slow->next;
-				fast = fast->next->next;
+			while (slow && fast && fast->next)
+			{
+						slow = slow->next;
+						fast = fast->next->next;
 
-				if (slow == fast)
-				{
-						slow = head;
-						while (slow != fast)
+						if (slow == fast)
 						{
-								slow = slow->next;
-								fast = fast->next;
+									slow = head;
+									while (slow != fast)
+									{
+											slow = slow->next;
+											fast = fast->next;
+									}
+									return (slow);
 						}
-						return (slow);
-				}
-		}
-		return (NULL);
+			}
+			return (NULL);
 }
