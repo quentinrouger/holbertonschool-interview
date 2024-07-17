abc8f88b32fbb94c5c9e8749e2fea83291992e4b
quentinrouger <quentin_rouger@hotmail.fr>
2024-07-17 09:54:07 +0200

    Fix: Betty coding style

--- double_circular_linked_list/0-add_node.c
+++ double_circular_linked_list/0-add_node.c
@@ -12,32 +12,32 @@ List *add_node_end(List **list, char *str)
 	List *new_node, *tail;
 
 	if (!list || !str)
-			return (NULL);
+		return (NULL);
 
 	new_node = malloc(sizeof(List));
 	if (!new_node)
-			return (NULL);
+		return (NULL);
 
 	new_node->str = strdup(str);
 	if (!new_node->str)
 	{
-			free(new_node);
-			return (NULL);
+		free(new_node);
+		return (NULL);
 	}
 
 	if (*list == NULL)
 	{
-			*list = new_node;
-			new_node->next = new_node;
-			new_node->prev = new_node;
+		*list = new_node;
+		new_node->next = new_node;
+		new_node->prev = new_node;
 	}
 	else
 	{
-			tail = (*list)->prev;
-			new_node->next = *list;
-			new_node->prev = tail;
-			tail->next = new_node;
-			(*list)->prev = new_node;
+		tail = (*list)->prev;
+		new_node->next = *list;
+		new_node->prev = tail;
+		tail->next = new_node;
+		(*list)->prev = new_node;
 	}
 
 	return (new_node);
@@ -56,33 +56,33 @@ List *add_node_begin(List **list, char *str)
 	List *new_node, *tail;
 
 	if (!list || !str)
-			return (NULL);
+		return (NULL);
 
 	new_node = malloc(sizeof(List));
 	if (!new_node)
-			return (NULL);
+		return (NULL);
 
 	new_node->str = strdup(str);
 	if (!new_node->str)
 	{
-			free(new_node);
-			return (NULL);
+		free(new_node);
+		return (NULL);
 	}
 
 	if (*list == NULL)
 	{
-			*list = new_node;
-			new_node->next = new_node;
-			new_node->prev = new_node;
+		*list = new_node;
+		new_node->next = new_node;
+		new_node->prev = new_node;
 	}
 	else
 	{
-			tail = (*list)->prev;
-			new_node->next = *list;
-			new_node->prev = tail;
-			tail->next = new_node;
-			(*list)->prev = new_node;
-			*list = new_node;
+		tail = (*list)->prev;
+		new_node->next = *list;
+		new_node->prev = tail;
+		tail->next = new_node;
+		(*list)->prev = new_node;
+		*list = new_node;
 	}
 
 	return (new_node);
