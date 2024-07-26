c74efd170d34239ff70cccfd74b31d081a712744
quentinrouger <quentin_rouger@hotmail.fr>
2024-07-26 17:10:47 +0200

    Fix Betty style

--- radix_sort/0-radix_sort.c
+++ radix_sort/0-radix_sort.c
@@ -10,15 +10,15 @@
  */
 int get_max(int *array, size_t size)
 {
-		int max = array[0];
-		size_t i;
+	int max = array[0];
+	size_t i;
 
-		for (i = 1; i < size; i++)
-		{
-				if (array[i] > max)
-						max = array[i];
-		}
-		return (max);
+	for (i = 1; i < size; i++)
+	{
+			if (array[i] > max)
+					max = array[i];
+	}
+	return (max);
 }
 
 /**
@@ -30,48 +30,49 @@ int get_max(int *array, size_t size)
  */
 void count_sort(int *array, size_t size, int exp)
 {
-		int *output = malloc(size * sizeof(int));
-		int count[10] = {0};
-		size_t i;
+	int *output = malloc(size * sizeof(int));
+	int count[10] = {0};
+	size_t i;
 
-		if (!output)
-				return;
+	if (!output)
+			return;
 
-		for (i = 0; i < size; i++)
-				count[(array[i] / exp) % 10]++;
+	for (i = 0; i < size; i++)
+			count[(array[i] / exp) % 10]++;
 
-		for (i = 1; i < 10; i++)
-				count[i] += count[i - 1];
+	for (i = 1; i < 10; i++)
+			count[i] += count[i - 1];
 
-		for (i = size; i > 0; i--)
-		{
-				output[count[(array[i - 1] / exp) % 10] - 1] = array[i - 1];
-				count[(array[i - 1] / exp) % 10]--;
-		}
+	for (i = size; i > 0; i--)
+	{
+			output[count[(array[i - 1] / exp) % 10] - 1] = array[i - 1];
+			count[(array[i - 1] / exp) % 10]--;
+	}
 
-		for (i = 0; i < size; i++)
-				array[i] = output[i];
+	for (i = 0; i < size; i++)
+			array[i] = output[i];
 
-		free(output);
+	free(output);
 }
 
 /**
- * radix_sort - Sorts an array of integers in ascending order using the Radix sort algorithm.
+ * radix_sort - Sorts an array of integers in ascending order
+ * using the Radix sort algorithm.
  * @array: The array to sort.
  * @size: The size of the array.
  */
 void radix_sort(int *array, size_t size)
 {
-		int max, exp;
+	int max, exp;
 
-		if (!array || size < 2)
-				return;
+	if (!array || size < 2)
+			return;
 
-		max = get_max(array, size);
+	max = get_max(array, size);
 
-		for (exp = 1; max / exp > 0; exp *= 10)
-		{
-				count_sort(array, size, exp);
-				print_array(array, size);
-		}
+	for (exp = 1; max / exp > 0; exp *= 10)
+	{
+			count_sort(array, size, exp);
+			print_array(array, size);
+	}
 }
